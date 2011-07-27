d5c5ee9d30a247652abe0a8180787c5d1b64b688
saket <saki007ster@gmail.com>
2011-07-27 16:51:54 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wbs1::Application.config.secret_token = 'a20b8f2935b15103ce1c72b4a7dc4af0b26b6f9f69799c885299a381124566cee6b6e4ced4986f29ed7269e687f1a2d23553f5a8a4a32dbc6a2be20d6e898e0b'
